d5b8add0c09757b748a97dcdb6f4e5a6c3d68a7e
guayj <guayj@430703c4-9ca8-11de-b798-49b661d4457b>
2009-09-01 13:44:10 +0000

    - Continued to wrap Array to be able to at least do basic things with it.

--- osgBoostPython/src/osg/array.cpp
+++ osgBoostPython/src/osg/array.cpp
@@ -7,24 +7,71 @@ using namespace boost::python;
 #include <osg/Array>
 #include <osg/MixinVector>
 
+#include <stdexcept>
 #include "ContainerUtils.h"
 
 using namespace osg;
 
+// http://cci.lbl.gov/cctbx_sources/scitbx/misc/positive_getitem_index.h
+template <typename IndexType, typename SizeType>
+SizeType positive_getitem_index(IndexType const& i, SizeType const& size, bool allow_i_eq_size=false, 
+                                const char* index_out_of_range = "Index out of range.")
+{
+    if (i >= 0)
+    {
+        SizeType result = static_cast<SizeType>(i);
+
+        if ( result > size ||
+             (result == size && !allow_i_eq_size))
+        {
+            throw std::out_of_range(index_out_of_range);
+        }
+        return result;
+    }
+    if (static_cast<SizeType>(-i) > size) 
+    {
+        throw std::out_of_range(index_out_of_range);
+    }
+    return size + i;
+}
+
 
 // http://cci.lbl.gov/cctbx_sources/scitbx/array_family/boost_python/flex_wrapper.h
-template<typename ContainerType>
+template<typename ContainerType,
+         typename GetitemReturnValuePolicy = return_value_policy<copy_non_const_reference> >
 struct ArrayWrapper
 {
-    //typedef ContainerType::ElementDataType ElementType;
+    typedef typename ContainerType::ElementDataType ElementType;
 
     typedef boost::python::class_<ContainerType, bases<Array>, ref_ptr<ContainerType> > class_t;
+
+    static ElementType& getitem_1d(ContainerType& container, long i)
+    {
+      //if (!a.check_shared_size()) raise_shared_size_mismatch();
+      std::size_t j = positive_getitem_index(i, container.size());
+      return container[j];
+    }
+
+    static void setitem_1d(ContainerType& container, long i, ElementType const& element)
+    {
+      //if (!a.check_shared_size()) raise_shared_size_mismatch();
+      std::size_t j = positive_getitem_index(i, container.size());
+      container[j] = element;
+    }
+
+    static void append(ContainerType& container, ElementType const& element)
+    {
+        container.push_back(element);
+    }
 
     static class_t wrap(const std::string& name)
     {
         class_t result(name.c_str());
         result
             .def("__len__", &ContainerType::size)
+            .def("__getitem__", getitem_1d, GetitemReturnValuePolicy())
+            .def("__setitem__", setitem_1d)
+            .def("append", append)
 
             // TODO: Not all are needed, but a few...
             // At least support whatever is needed to give a python-esque interface.
