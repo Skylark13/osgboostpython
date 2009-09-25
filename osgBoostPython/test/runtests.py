c0afa5809009812aa2846ae599ea5abcbedceb6e
jean-sebastien.guay@cm-labs.com <Syklark13>
2009-09-25 03:01:13 +0000

    - Removed Eric4 project, will recreate one in build/ to include tests, examples, etc. - Removed old commented code in runtests.py.

--- osgBoostPython/test/runtests.py
+++ osgBoostPython/test/runtests.py
@@ -3,7 +3,7 @@
 # Shouldn't have to do this, but for some reason Eric doesn't add PYTHONPATH to sys.path by default...
 import sys
 import os
-sys.path = os.environ.get("PYTHONPATH").split(";") + sys.path[:] 
+sys.path = os.environ.get("PYTHONPATH").split(";") + sys.path[:]
 #print "sys.path=", sys.path
 
 # http://docs.python.org/library/unittest.html
@@ -14,7 +14,7 @@ import imp
 
 print os.getcwd()
 
-testsToSkip = ['test_osgViewer','test_all'] #needs user interaction
+testsToSkip = ['test_osgViewer'] #needs user interaction
 suites = []
 
 for t in glob.glob('test/*/test_*.py'):
@@ -23,31 +23,14 @@ for t in glob.glob('test/*/test_*.py'):
     if modulename in testsToSkip:
         continue
     m = imp.load_source(modulename, t )
-    
+
     for module_member in dir(m):
         handle = getattr(m, module_member)
         try:
             if issubclass(handle, unittest.TestCase):
                 suites.append(unittest.TestLoader().loadTestsFromTestCase( handle ) )
-                print 'Found test: ' + module_member 
+                print 'Found test: ' + module_member
         except:
             pass
 alltests = unittest.TestSuite(suites)
 unittest.TextTestRunner(verbosity = 2).run(alltests)
-
-#
-## Import all test modules
-#import test_osg
-#import test_osgDB
-#import test_osgGA
-#import test_osgViewer
-#
-#osgTestSuite = unittest.TestLoader().loadTestsFromTestCase(test_osg.osgTest)
-#osgDBTestSuite = unittest.TestLoader().loadTestsFromTestCase(test_osgDB.osgDBTest)
-#osgGATestSuite = unittest.TestLoader().loadTestsFromTestCase(test_osgGA.osgGATest)
-#osgViewerTestSuite = unittest.TestLoader().loadTestsFromTestCase(test_osgViewer.osgViewerTest)
-#
-#alltests = unittest.TestSuite([osgTestSuite, osgDBTestSuite, osgGATestSuite, osgViewerTestSuite])
-#
-## Run all tests
-#unittest.TextTestRunner().run(alltests)
