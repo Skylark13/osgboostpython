8ed4f58f6258a00e21dcc2a7f845fe33b7b037a3
jean-sebastien.guay@cm-labs.com <Syklark13>
2010-08-15 19:47:52 +0000

    - Seems Python doesn't like when there's an accented character in the copyright header comment...

--- osgBoostPython/lib/osgUtil/__init__.py
+++ osgBoostPython/lib/osgUtil/__init__.py
@@ -1,7 +1,7 @@
 #
 # This source file is part of the osgBoostPython library
 # 
-# Copyright (C) 2009-2010 Jean-Sébastien Guay
+# Copyright (C) 2009-2010 Jean-Sebastien Guay
 # 
 # This program is free software; you can redistribute it and/or modify it under
 # the terms of the GNU Lesser General Public License as published by the Free Software
