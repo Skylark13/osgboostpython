9791e57f5aa439ca0ae190e30b68d3e6f049a734
jean-sebastien.guay@cm-labs.com <jean-sebastien.guay@cm-labs.com@430703c4-9ca8-11de-b798-49b661d4457b>
2010-08-15 19:06:28 +0000

    - Added copyright header to all source files.

--- trunk/osgBoostPython/src/osg/osg.cpp
+++ trunk/osgBoostPython/src/osg/osg.cpp
@@ -1,3 +1,19 @@
+/*
+* This source file is part of the osgBoostPython library
+* 
+* Copyright (C) 2009-2010 Jean-Sébastien Guay
+* 
+* This program is free software; you can redistribute it and/or modify it under
+* the terms of the GNU Lesser General Public License as published by the Free Software
+* Foundation; either version 3 of the License, or (at your option) any later
+* version.
+* 
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+* http://www.gnu.org/copyleft/lesser.txt.
+*/
+
 // Good sources of information:
 // http://wiki.python.org/moin/boost.python
 // http://www.boost.org/doc/libs/1_36_0/libs/python/doc/tutorial/doc/html/index.html
