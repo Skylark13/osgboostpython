72b0e6c8a7b15f3df3327bd09dd90f7be9ad2721
jean-sebastien.guay@cm-labs.com <jean-sebastien.guay@cm-labs.com@430703c4-9ca8-11de-b798-49b661d4457b>
2010-08-15 18:49:32 +0000

    - Removed unnecessary linker paths from the project file. - Simplified and commented the main.cpp.

--- trunk/osgBoostPython/examples/embedded/main.cpp
+++ trunk/osgBoostPython/examples/embedded/main.cpp
@@ -1,3 +1,5 @@
+// This code initially from http://wiki.python.org/moin/boost.python/EmbeddingPython
+// with some modifications.
 #include <iostream>
 #include <string>
 
@@ -7,33 +9,35 @@ using namespace boost::python;
 
 int main( int argc, char ** argv )
 {
-    bool exit = false;
+    Py_Initialize();
 
-    while (!exit)
+    object main_module((
+        handle<>(borrowed(PyImport_AddModule("__main__")))));
+
+    object main_namespace = main_module.attr("__dict__");
+
+    std::string command;
+    while (true)
     {
-        try
+        // Print a prompt and read the command from the user.
+        std::cout << "> ";
+        getline(std::cin, command);
+
+        // Try to execute the command.
+        try 
         {
-            Py_Initialize();
+            handle<> ignored(( PyRun_String( command.c_str(),
+                                             Py_file_input,
+                                             main_namespace.ptr(),
+                                             main_namespace.ptr() ) ));
 
-            object main_module((
-                handle<>(borrowed(PyImport_AddModule("__main__")))));
-
-            object main_namespace = main_module.attr("__dict__");
-
-            while (true)
+            // I just want to be a bit more lenient that the python 
+            // interpreter, which nags you to type exit() or quit()
+            // when it's already pretty clear you want to exit the
+            // interpreter...
+            if (command == "exit" || command == "quit")
             {
-                std::string command;
-                std::cout << "> ";
-                getline(std::cin, command);
-                handle<> ignored(( PyRun_String( command.c_str(),
-                                                 Py_file_input,
-                                                 main_namespace.ptr(),
-                                                 main_namespace.ptr() ) ));
-                if (command == "exit")
-                {
-                    exit = true;
-                    break;
-                }
+                break;
             }
         }
         catch( error_already_set )
