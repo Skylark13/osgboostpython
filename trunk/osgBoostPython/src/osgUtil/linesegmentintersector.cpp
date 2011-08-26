511c4d8452ecbc98ddcda277aeb46882b4471440
alex.r.olivas@gmail.com <alex.r.olivas@gmail.com@430703c4-9ca8-11de-b798-49b661d4457b>
2011-08-26 06:26:05 +0000

    cleaned things up a bit.  no big changes.

--- trunk/osgBoostPython/src/osgUtil/linesegmentintersector.cpp
+++ trunk/osgBoostPython/src/osgUtil/linesegmentintersector.cpp
@@ -16,48 +16,54 @@ using namespace boost::python;
 
 #include "defaults.h"
 
+Vec3 const & (LineSegmentIntersector::Intersection::*get_loc_int_norm)(void) const = &::LineSegmentIntersector::Intersection::getLocalIntersectNormal;
+Vec3d const & ( LineSegmentIntersector::Intersection::*get_loc_int_pt )(void) const = &::LineSegmentIntersector::Intersection::getLocalIntersectPoint;
+Vec3 ( LineSegmentIntersector::Intersection::*get_world_int_norm )(void) const =  &::LineSegmentIntersector::Intersection::getWorldIntersectNormal;
+Vec3d ( LineSegmentIntersector::Intersection::*get_world_int_pt )(void) const = &::LineSegmentIntersector::Intersection::getWorldIntersectPoint;    
+    
 void export_linesegmentintersector() {
  
-    class_<LineSegmentIntersector, bases<Intersector>, ref_ptr<LineSegmentIntersector>, boost::noncopyable >lsi( "LineSegmentIntersector",  init< Intersector::CoordinateFrame, double, double >(( arg("cf"), arg("x"), arg("y") )) );
+  class_<LineSegmentIntersector, bases<Intersector>, ref_ptr<LineSegmentIntersector>, boost::noncopyable >
+      lsi( "LineSegmentIntersector",  init< Intersector::CoordinateFrame, double, double >(( arg("cf"), arg("x"), arg("y") )) );
 
     scope in_LineSegmentIntersector(lsi);
 
-        class_< LineSegmentIntersector::Intersection >( "Intersection", init< >() )    
-            .def(  "getLocalIntersectNormal", (Vec3 const & ( LineSegmentIntersector::Intersection::* )(  ) const)( &::LineSegmentIntersector::Intersection::getLocalIntersectNormal ), return_value_policy< copy_const_reference >() )    
-            .def(  "getLocalIntersectPoint", (Vec3d const & ( LineSegmentIntersector::Intersection::* )(  ) const)( &::LineSegmentIntersector::Intersection::getLocalIntersectPoint ), return_value_policy< copy_const_reference >() )    
-            .def(  "getWorldIntersectNormal", (Vec3 ( LineSegmentIntersector::Intersection::* )(  ) const)( &::LineSegmentIntersector::Intersection::getWorldIntersectNormal ) )    
-            .def(  "getWorldIntersectPoint", (Vec3d ( LineSegmentIntersector::Intersection::* )(  ) const)( &::LineSegmentIntersector::Intersection::getWorldIntersectPoint ) )    
-            .def( self < self )    
-            .def_readwrite( "drawable", &LineSegmentIntersector::Intersection::drawable )    
-            .def_readwrite( "indexList", &LineSegmentIntersector::Intersection::indexList )    
-            .def_readwrite( "localIntersectionNormal", &LineSegmentIntersector::Intersection::localIntersectionNormal )    
-            .def_readwrite( "localIntersectionPoint", &LineSegmentIntersector::Intersection::localIntersectionPoint )    
-            .def_readwrite( "matrix", &LineSegmentIntersector::Intersection::matrix )    
-            .def_readwrite( "nodePath", &LineSegmentIntersector::Intersection::nodePath )    
-            .def_readwrite( "primitiveIndex", &LineSegmentIntersector::Intersection::primitiveIndex )    
-            .def_readwrite( "ratio", &LineSegmentIntersector::Intersection::ratio )    
-            .def_readwrite( "ratioList", &LineSegmentIntersector::Intersection::ratioList );
+    class_< LineSegmentIntersector::Intersection >( "Intersection", init< >() )    
+      .def(  "getLocalIntersectNormal", get_loc_int_norm, return_value_policy< copy_const_reference >() )    
+      .def(  "getLocalIntersectPoint", get_loc_int_pt, return_value_policy< copy_const_reference >() )    
+      .def(  "getWorldIntersectNormal", get_world_int_norm )    
+      .def(  "getWorldIntersectPoint", get_world_int_pt )    
+      .def( self < self )    
+      .def_readwrite( "drawable", &LineSegmentIntersector::Intersection::drawable )    
+      .def_readwrite( "indexList", &LineSegmentIntersector::Intersection::indexList )    
+      .def_readwrite( "localIntersectionNormal", &LineSegmentIntersector::Intersection::localIntersectionNormal )    
+      .def_readwrite( "localIntersectionPoint", &LineSegmentIntersector::Intersection::localIntersectionPoint )    
+      .def_readwrite( "matrix", &LineSegmentIntersector::Intersection::matrix )    
+      .def_readwrite( "nodePath", &LineSegmentIntersector::Intersection::nodePath )    
+      .def_readwrite( "primitiveIndex", &LineSegmentIntersector::Intersection::primitiveIndex )    
+      .def_readwrite( "ratio", &LineSegmentIntersector::Intersection::ratio )    
+      .def_readwrite( "ratioList", &LineSegmentIntersector::Intersection::ratioList );
         
     lsi
-        .def( init< Intersector::CoordinateFrame, Vec3d const &, Vec3d const & >(( arg("cf"), arg("start"), arg("end") )) )
-        .def( init< Vec3d const &, Vec3d const & >(( arg("start"), arg("end") ) ))        
-        .def( init< Intersector::CoordinateFrame, double, double >(( arg("cf"), arg("x"), arg("y") ) ))        
-        //.def("leave", &LineSegmentIntersector::leave)
-        //.def("operator=", LineSegmentIntersector_operator=1)
-        .def("insertIntersection", &LineSegmentIntersector::insertIntersection)
-        .def("setStart", &LineSegmentIntersector::setStart)
-        .def("containsIntersections", &LineSegmentIntersector::containsIntersections)
-        //.def("enter", &LineSegmentIntersector::enter)
-        //.def("clone", &LineSegmentIntersector::clone)
-        //.def("operator=", Intersection_operator=2)
-        .def("intersect", &LineSegmentIntersector::intersect)
-        .def("setEnd", &LineSegmentIntersector::setEnd)
-        .def("getEnd", &LineSegmentIntersector::getEnd, osgBoostPython::default_const_reference_policy())
-        .def("getStart", &LineSegmentIntersector::getStart, osgBoostPython::default_const_reference_policy())
-        .def("reset", &LineSegmentIntersector::reset)
-        //.def("operator<", &LineSegmentIntersector::Intersection::operator<)
-        .def("getFirstIntersection", &LineSegmentIntersector::getFirstIntersection, osgBoostPython::default_value_policy())
-        .def("getIntersections", &LineSegmentIntersector::getIntersections, osgBoostPython::default_reference_policy()) //compiles, but doesn't work.
-    ;
+      .def( init< Intersector::CoordinateFrame, Vec3d const &, Vec3d const & >(( arg("cf"), arg("start"), arg("end") )) )
+      .def( init< Intersector::CoordinateFrame, double, double >(( arg("cf"), arg("x"), arg("y") ) ))        
+      //.def("leave", &LineSegmentIntersector::leave)
+      //.def("operator=", LineSegmentIntersector_operator=1)
+      .def("insertIntersection", &LineSegmentIntersector::insertIntersection)
+      .def("setStart", &LineSegmentIntersector::setStart)
+      .def("containsIntersections", &LineSegmentIntersector::containsIntersections)
+      //.def("enter", &LineSegmentIntersector::enter)
+      //.def("clone", &LineSegmentIntersector::clone)
+      //.def("operator=", Intersection_operator=2)
+      .def("intersect", &LineSegmentIntersector::intersect)
+      .def("setEnd", &LineSegmentIntersector::setEnd)
+      .def("getEnd", &LineSegmentIntersector::getEnd, osgBoostPython::default_const_reference_policy())
+      .def("getStart", &LineSegmentIntersector::getStart, osgBoostPython::default_const_reference_policy())
+      .def("reset", &LineSegmentIntersector::reset)
+      //.def("operator<", &LineSegmentIntersector::Intersection::operator<)
+      .def("getFirstIntersection", &LineSegmentIntersector::getFirstIntersection, osgBoostPython::default_value_policy())
+      .def("getIntersections", &LineSegmentIntersector::getIntersections, osgBoostPython::default_reference_policy()) //compiles, but doesn't work.
+      .def( init< Vec3d const &, Vec3d const & >(( arg("start"), arg("end") ) ))        
+      ;
     
 }
